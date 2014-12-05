409ebe05a23731450356eb7a95b4c8e0607ff4e4
Andrew Taylor <Winstonia@Andrews-MacBook-Pro.local>
2014-12-04 21:32:58 -1000

    changes to totdo

--- public/js/todo.js
+++ public/js/todo.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
-	alert('aloha');
+	$('#input').keydown(function(e){
+		if(e.which == 13) {
+			var value = $(this).val();//accessing a string variable (below)
+			$('#to_do_list').append('<li>'+value+'</li>'); 
+		}
+	});
+
 });
