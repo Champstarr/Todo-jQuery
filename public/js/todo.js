d0290e5237a9e5daf6eeb41dc2cf00c5d2047a22
Andrew Taylor <Winstonia@Andrews-MacBook-Pro.local>
2014-12-04 21:43:49 -1000

    changes to todo

--- public/js/todo.js
+++ public/js/todo.js
@@ -2,8 +2,11 @@ $(document).ready(function(){
 	$('#input').keydown(function(e){
 		if(e.which == 13) {
 			var value = $(this).val();//accessing a string variable (below)
-			$('#to_do_list').append('<li>'+value+'</li>'); 
+			$('#to_do_list').append('<li><input type="checkbox">'+value+'</li>'); 
+			$(this).val('');
 		}
 	});
+
+
 
 });
