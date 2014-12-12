626af8a300ea2d048a9c2ee5b5f0506a6a1bf8c4
Andrew Taylor <Winstonia@Andrews-MacBook-Pro.local>
2014-12-11 21:35:32 -1000

    changes to todo

--- server/app.js
+++ server/app.js
@@ -1,12 +1,25 @@
 var express = require('express')
 var app = express()
 var bodyParser = require('body-parser');
+var fs = require("fs");
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+ function saveTodoList(content){
+  fs.writeFile('./public/todo_save.txt', content, function(err){
+    if (err) return console.log(err);
+    console.log("successfully saved todo_save.json");
+
+  });
+
+
+ }
+
  app.post('/save', function(req,res){
   console.log(req.body);
+  
+  saveTodoList(req.body.list_to_save);
     res.send("puppies")
 
   })
