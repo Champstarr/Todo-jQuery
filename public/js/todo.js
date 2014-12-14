3d63163259faff781b0cbfa62946985846975abc
Andrew Taylor <Winstonia@Andrews-MacBook-Pro.local>
2014-12-13 17:23:58 -1000

    changes to todo

--- public/js/todo.js
+++ public/js/todo.js
@@ -1,32 +1,53 @@
 $(document).ready(function(){
 	//auto-load the save file
 	$.get("/item", function(data){
-		var list_items = data;
-		for (var i = 0; i < list_items.length; i++) {
-			var new_item = $("<li></li>");
-			new_item.addClass("li_item");
+		for (var i = 0; i < data.length; i++) {
+			// var new_item = $("<li></li>");
+			// new_item.addClass("li_item");
 			var new_checkbox = $("<input type='checkbox'>");
-			new_item.append(new_checkbox);
-			new_item.append("<span>"+list_items[i].title+"</span>");
-			$("ul#to_do_list").append(new_item);
+			var list_items = $('<li>', {
+				class: "li_item",
+				"data-object-id": data[i]._id
+			});
+			var list_delete = $('<button>', {
+				text: "[delete]",
+				click: function (e){
+					var button = $(e.currentTarget);
+					var object_id = button.closest("li").data ("object-id");
+
+					$.ajax('/items/' + object_id, {
+						type: "DELETE",
+						success: function (data) {
+							console.log('data',data);
+						}
+					});
+				}
+			});
+
+			if(data[i].completed){
+				new_checkbox.prop("checked", data[i].completed);
+			}
+			list_items.append(list_delete);
+			list_items.append(new_checkbox);
+			list_items.append("<span>" + data[i].title + "</span>");
+			$("ul#to_do_list").append(list_items);
 		}
-	});
+	}); //$.get()
 	
 	$('#input').keydown(function(e){
 		if(e.which == 13) {
 			var value = $(this).val();//accessing a string variable (below)
 			$('#to_do_list').append('<li class="li_item"><input type="checkbox" class="checkbox"><span>'+value+'</span></li>'); 
-			$(this).val('');
 			var post_data = {
-    		new_item : {
-        title : "my task",
-        checked : false
-    }
-}
+    			new_item : {
+        	title : $("#input").val(),
+        	checked : false
+    		}
+			}
+			$(this).val('');
 
-$.post('/item', post_data) 
+			$.post('/item', post_data) 
 		}
-
 	});
 	$('body').on("click", ".checkbox", function(e){
 		var status = this.checked 
--- server/app.js
+++ server/app.js
@@ -1,7 +1,9 @@
 var express = require('express')
 var app = express()
 var bodyParser = require('body-parser');
-var MongoClient = require('mongodb').MongoClient;
+var mongodb = require('mongodb');
+var MongoClient = mongodb.MongoClient;
+var ObjectID = mongodb.ObjectID;
 var fs = require('fs');
 
 app.use(express.static('public'));
@@ -32,7 +34,7 @@ app.post('/item', function(req,res){
     collection.insert(req.body.new_item, function(err, item) {
       // Show the item that was just inserted; contains the _id field
       // Note that it is an array containing a single object
-      console.log(item[0]._id);
+      console.log(item[0]);
       res.send(item[0]._id);
 
       // Log the number of items in the collection
@@ -42,7 +44,7 @@ app.post('/item', function(req,res){
 
       // Locate all the entries using find
       collection.find().toArray(function(err, results) {
-        console.log(results);
+        // console.log(results);
 
         // Close the db connection
         db.close();
@@ -63,12 +65,39 @@ app.post('/item', function(req,res){
     collection.find().toArray(function(err, results){
       res.send(results);
 
+      db.close();
+
 
     });
 
   });
 
- })
+ });
+
+ app.delete('/items/:id', function(req,res){
+  // connect_to_db( function (collection) {
+  //   var _id = req.params.id;
+  //   collection.remove({"_id": new objectID(_id)}, function (err,result) {
+  //     if(err) throw err;
+  //     res.json({ success: "success"});
+  //     collection.db.close();
+  //   });
+  // }); 
+   MongoClient.connect('mongodb://localhost:27017/todosdb', function(err, db) {
+    if (err) {
+      throw err;
+    }
+
+    // Find the collection todos (or create it if it doesn't already exist)
+    var collection = db.collection('todos');
+    var _id = req.params.id;
+    collection.remove({"_id": new ObjectID(_id)}, function (err,result) {
+      if(err) throw err;
+      res.json({ success: "success"});
+      collection.db.close();
+    });
+  });
+ });
 
 var server = app.listen(3000, function () {
 
